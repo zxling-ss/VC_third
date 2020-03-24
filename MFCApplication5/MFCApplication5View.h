79ebe7d3b794838f1820178a49bec0203acf9557
zxling-ss <57402329+zxling-ss@users.noreply.github.com>
2020-03-24 19:48:08 +0800

    Add files via upload

---
+++ MFCApplication5/MFCApplication5View.h
@@ -0,0 +1,53 @@
+﻿
+// MFCApplication5View.h: CMFCApplication5View 类的接口
+//
+
+#pragma once
+
+
+class CMFCApplication5View : public CView
+{
+protected: // 仅从序列化创建
+	CMFCApplication5View() noexcept;
+	DECLARE_DYNCREATE(CMFCApplication5View)
+
+// 特性
+public:
+	CMFCApplication5Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CMFCApplication5View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+};
+
+#ifndef _DEBUG  // MFCApplication5View.cpp 中的调试版本
+inline CMFCApplication5Doc* CMFCApplication5View::GetDocument() const
+   { return reinterpret_cast<CMFCApplication5Doc*>(m_pDocument); }
+#endif
+
